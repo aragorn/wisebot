7de0fb9bf364fd0a734af4630d0ddf2dff854524
aragorn <aragorn>
2005-07-09 13:57:07 +0000

    removed duplicated global variables

--- modules/mod_ifs/test.c
+++ modules/mod_ifs/test.c
@@ -6,9 +6,6 @@
 static char path[]="../../dat/test/ifs";
 
 /******************************************************************************/
-char gSoftBotRoot[MAX_PATH_LEN] = SERVER_ROOT;
-char gErrorLogFile[MAX_PATH_LEN] = DEFAULT_ERROR_LOG_FILE;
-char gQueryLogFile[MAX_PATH_LEN] = DEFAULT_QUERY_LOG_FILE;
 module *static_modules;
 /******************************************************************************/
 
