14918e8e7aaf49135a6ad84596ae496bc2552925
blueend <blueend>
2006-03-06 10:42:30 +0000

    mod_qp2 추가, comment(summary) 추출시 disk 한번만 읽도록 수정

--- modules/mod_qp/mod_qp.h
+++ modules/mod_qp/mod_qp.h
@@ -84,6 +84,25 @@ struct request_t{
 	int sb4error;
 };
 
+
+///////////////////////////////////////////////////////////
+// 설정파일에서 읽어온 필드의 속성
+enum field_type {
+    NONE,           // 아무처리하지 않음.
+	RETURN,         // 단순출력
+    SUM,            // 요약출력	
+	SUMANY,         // 단어가 없더라도 요약출력
+};
+
+// 필드명, 필드 속성
+typedef struct {
+    char name[SHORT_STRING_SIZE];
+    int type;       // enum field_type
+    int id;         // field id
+} field_info_t;
+///////////////////////////////////////////////////////////
+
+
 /* stack related stuff */
 typedef struct {
 	uint32_t size;
