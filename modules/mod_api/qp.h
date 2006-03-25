f2d9edc664a01bdce07265e4b19831d278e6d83c
blueend <blueend>
2006-03-25 08:14:23 +0000

    WiseBot/Upgrade/Structure 적용, structure를 api로 이동

--- modules/mod_api/qp.h
+++ modules/mod_api/qp.h
@@ -7,6 +7,11 @@ typedef struct filter_t filter_t;
 typedef struct rank_t rank_t;
 typedef struct index_list_t index_list_t;
 typedef struct group_result_t group_result_t;
+typedef struct agent_doc_hits_t agent_doc_hits_t;
+typedef struct agent_request_t agent_request_t;
+typedef struct agent_light_info_t agent_light_info_t;
+typedef struct agent_full_info_t agent_full_info_t;
+typedef struct sort_base_t sort_base_t;
 
 #include "docattr.h"
 #include "indexer.h"
@@ -23,6 +28,18 @@ typedef struct group_result_t group_result_t;
 #define MAX_INCOMPLETE_INDEX_LIST_POOL	(30) 
 /* MAX_DOC_HITS_SIZE: size of the document set retrieved from VRF */
 #define MAX_DOC_HITS_SIZE (1200000)
+#define MAX_GROUP_RESULT (200)
+
+//FIXME move this definition to appropriate place
+#define MAX_AGENT_SEARCH_ROW_NUM    (1000)
+#define MAX_SEARCH_LIST_COUNT 		(100)
+
+// 최대 하위 노드 수
+// 4bit로 표현할수 있는 수.
+#define MAX_SEARCH_NODE (1 << 4) 
+#define DEFAULT_PORT "8000"
+
+#define MAX_AGENT_DOC_HITS_COUNT (MAX_SEARCH_NODE * MAX_AGENT_SEARCH_ROW_NUM)
 
 enum index_list_type {
 	EXCLUDE,
@@ -39,6 +56,11 @@ enum requesttype {
 	NO_HANDLER
 };
 
+enum sortarraytype {
+    INDEX_LIST,
+    AGENT_INFO,
+};
+
 // 통합검색 지원
 struct group_result_t {
 	char field[20];
@@ -46,9 +68,50 @@ struct group_result_t {
 	int count;
 };
 
-#define MAX_GROUP_RESULT 200
+/* docattr sorting시에 type에 따라 casting을 다르게 하기 위해 */
+struct sort_base_t {
+    enum sortarraytype type;
+};
+
+struct agent_doc_hits_t {
+    uint32_t node_id;
+    docattr_t docattr;
+	uint32_t  relevancy;
+	doc_hit_t doc_hits;
+	char comments[LONG_LONG_STRING_SIZE];
+};
+
+struct agent_light_info_t {
+    sort_base_t sort_base;     // 반드시 처음에 선언.
+
+	uint32_t total_cnt;                
+	uint32_t recv_cnt;                // agent_doc_hits의 갯수
+    agent_doc_hits_t* agent_doc_hits[MAX_AGENT_DOC_HITS_COUNT];
+	char word_list[STRING_SIZE];
+
+	group_result_t group_result[MAX_GROUP_RESULT];
+};
+
+struct agent_request_t {
+    char query[MAX_QUERY_STRING_SIZE];
+    char at[MAX_ATTR_STRING_SIZE];
+    char at2[MAX_ATTR_STRING_SIZE];
+    char gr[MAX_GROUP_STRING_SIZE];
+    char sh[MAX_SORT_STRING_SIZE];
+	int lc;
+	int pg;
+	int ft;
+
+	//전송해야할 문서 건수
+	int send_cnt;
+	// 전송을 시작해야할 위치
+	int send_first;
+	agent_light_info_t ali;
+};
+
 
 struct index_list_t {
+    sort_base_t sort_base;
 	index_list_t *prev; /* for managing free index list */
 	index_list_t *next;
 
@@ -91,8 +154,9 @@ struct request_t {
 
 SB_DECLARE_HOOK(int, qp_init,(void))
 SB_DECLARE_HOOK(int, qp_light_search, (void* word_db, request_t *r))
+SB_DECLARE_HOOK(int, qp_agent_info_sort, (agent_request_t *r))
 SB_DECLARE_HOOK(int, qp_full_search, (void* word_db, request_t *r))
-SB_DECLARE_HOOK(int, qp_abstract_info, (request_t *r))
+SB_DECLARE_HOOK(int, qp_abstract_info, (agent_request_t *r))
 SB_DECLARE_HOOK(int, qp_full_info, (request_t *r))
 SB_DECLARE_HOOK(int, qp_finalize_search, (request_t *r))
 
