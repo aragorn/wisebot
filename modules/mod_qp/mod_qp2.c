f40893d7d69750c7de5f7be61f3c9f2a89b3bc7e
blueend <blueend>
2006-05-25 12:29:42 +0000

    memfile_appendF문제 빈문열일경우엔 출력하지 않도록

--- modules/mod_qp/mod_qp2.c
+++ modules/mod_qp/mod_qp2.c
@@ -2129,11 +2129,13 @@ static int get_comment(request_t* req, doc_hit_t* doc_hits, select_list_t* sl, c
 						return FAIL;
 					}
 				} else {
-					rv = memfile_appendF(buffer, " %s ", field_value);
-					if(rv < 0) {
-						MSG_RECORD(&req->msg, error, "can not appendF memfile");
-						memfile_free(buffer);
-						return FAIL;
+					if(strlen(field_value) != 0) {
+						rv = memfile_appendF(buffer, "%s", field_value);
+						if(rv < 0) {
+							MSG_RECORD(&req->msg, error, "can not appendF memfile");
+							memfile_free(buffer);
+							return FAIL;
+						}
 					}
 				}
 			}
@@ -2175,11 +2177,13 @@ static int get_comment(request_t* req, doc_hit_t* doc_hits, select_list_t* sl, c
 							return FAIL;
 						}
 					} else {
-						rv = memfile_appendF(buffer, " %s ", summary);
-						if(rv < 0) {
-							MSG_RECORD(&req->msg, error, "can not appendF memfile");
-							memfile_free(buffer);
-							return FAIL;
+					    if(strlen(field_value) != 0) {
+							rv = memfile_appendF(buffer, "%s", summary);
+							if(rv < 0) {
+								MSG_RECORD(&req->msg, error, "can not appendF memfile");
+								memfile_free(buffer);
+								return FAIL;
+							}
 						}
 					}
 				}
