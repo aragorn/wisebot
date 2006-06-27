dcc433e992801a7607f2424e356529a1972c7824
chaeyk <chaeyk>
2006-06-27 10:23:03 +0000

    line蹂꾨줈 grouping

--- include/stopwatch.h
+++ include/stopwatch.h
@@ -14,6 +14,7 @@
 #  endif
 
 static uint64_t    time_lengths[MAX_MARK];
+static int         time_lines[MAX_MARK]; // time_mark()를 실행한 source line
 static const char* time_names[MAX_MARK];
 static int         time_idx;
 static uint64_t    time_old;
@@ -36,31 +37,44 @@ static void _time_start()
 	time_old = tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
-static void _time_mark(const char* name)
+static void _time_mark(const char* name, int line)
 {
 	uint64_t time_new;
 	struct timeval tv;
 
-	if ( time_idx >= MAX_MARK ) {
-		warn("too many time mark. max is %d", MAX_MARK);
-		return;
+	int idx=0, i;
+	for ( i=0; i<time_idx; i++ ) {
+		if ( time_lines[i] == line ) {
+			idx = i;
+			break;
+		}
+	}
+
+	if ( i == time_idx ) {
+		if ( i >= MAX_MARK ) {
+			warn("too many time mark. max is %d", MAX_MARK);
+			return;
+		}
+
+		idx = i;
+		time_idx++;
 	}
 
 	gettimeofday(&tv, NULL);
 	time_new = tv.tv_sec * 1000000 + tv.tv_usec;
 
-	time_names[time_idx] = name;
-	time_lengths[time_idx] += time_new - time_old;
+	time_names[idx] = name;
+	time_lines[idx] = line;
+	time_lengths[idx] += time_new - time_old;
 
 	time_old = time_new;
-	time_idx++;
 }
 
 static void _time_status()
 {
 	if ( time_count >= TIME_COUNT ) {
 		int i;
-		for ( i = 0; i < MAX_MARK && time_names[i] != NULL; i++ ) {
+		for ( i = 0; i < time_idx; i++ ) {
 			info("%-20s: %'10" PRIu64 " usec", time_names[i], time_lengths[i]);
 		}
 		time_count = -1;
@@ -68,7 +82,7 @@ static void _time_status()
 }
 
 #  define time_start() _time_start()
-#  define time_mark(name) _time_mark(name)
+#  define time_mark(name) _time_mark(name, __LINE__)
 #  define time_status() _time_status()
 #else
 #  define time_start()
