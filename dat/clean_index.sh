6bf2d6be3673661ce50d3e840d8e6af633e13232
blueend <blueend>
2008-08-14 06:07:37 +0000

    검색엔진 실행중에는 실행할 수 없도록 수정

--- dat/clean_index.sh
+++ dat/clean_index.sh
@@ -2,6 +2,48 @@
 #$Id$
 
 set -x
+
+SOFTBOTD_PID_FILE=../logs/softbotd.pid
+PS=ps
+
+is_running()
+{
+    # RETURN VALUE
+    # > 0 : PID of the process
+    #   0 : is not running
+    #  -1 : empty pid file
+    #  -2 : no pid file
+    local PID_FILE=$1
+
+    if [ ! -r "$PID_FILE" ]; then
+        # no pid file
+        echo "-2"
+        return -2
+    fi
+
+    local PID=`cat $PID_FILE 2>/dev/null`
+
+    if [ ! -n "$PID" ]; then
+        # empty pid file
+        echo "-1"
+        return -1
+    fi
+
+    kill -0 $PID > /dev/null 2>&1
+
+    if [ $? != "0" ]; then
+        # no such process
+        echo "0"
+        return 0
+    fi
+
+    echo "$PID"
+    return 1
+}
+
+PID=`is_running "$SOFTBOTD_PID_FILE"`
+(( PID > 0 )) && printf "[ERROR: softbotd process is running($PID)] " && exit 1
+
 dir=`pwd`
 reldir=`basename $dir`
 if [ ! "$reldir" = "dat" -a ! "$reldir" = "var" ]
